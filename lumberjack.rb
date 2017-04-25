f696480272ef56cc2bb9d0491a116d5fc78ba4ba
Chris Penner <christopher.penner@gmail.com>
2017-04-24 18:07:45 -0600

    simplify lumberjack formula

--- lumberjack.rb
+++ lumberjack.rb
@@ -1,13 +1,14 @@
 class Lumberjack < Formula
   desc "interactive cli log watcher"
   homepage "https://github.com/ChrisPenner/lumberjack"
-  url "https://github.com/ChrisPenner/LumberJack/releases/download/v0.1.2/lumberjack_darwin_amd64"
+  url "https://github.com/ChrisPenner/LumberJack/releases/download/v0.1.2/lumberjack_darwin_amd64", :using => :nounzip
   sha256 "140853bc003466d0387dfbeba8e15a30e29b253ace35934d1b81f0a41b75af9d"
 
   bottle :unneeded
 
   def install
-    bin.install "lumberjack_darwin_amd64"
+    mv "lumberjack_darwin_amd64", "lumberjack"
+    bin.install "lumberjack"
   end
 
   test do
