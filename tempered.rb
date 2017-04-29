8a4488652afc585c25f716dd83e3d647d8d9e731
Chris Penner <christopher.penner@gmail.com>
2017-04-29 16:35:34 -0600

    Update tempered to v0.2.0

--- tempered.rb
+++ tempered.rb
@@ -1,8 +1,8 @@
 class Tempered < Formula
   desc "A dead-simple templating utility for simple shell interpolation"
   homepage "https://github.com/ChrisPenner/tempered"
-  url "https://github.com/ChrisPenner/tempered/releases/download/v0.1.0/tempered-v0.1.0-osx.tar.gz"
-  sha256 "9241be80db128ddcfaf9d2fc2520d22aab47935bcabc117ed874c627c0e1e0be"
+  url "https://github.com/ChrisPenner/tempered/releases/download/v0.2.0/tempered-v0.2.0-osx.tar.gz"
+  sha256 "8ed635fce8e11bcd9cdb3302c28dfb38327333dfee0b28c071b0f19ddb428449"
 
   bottle :unneeded
 
