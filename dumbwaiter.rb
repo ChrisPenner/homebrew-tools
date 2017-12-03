6f0fd6355f0081ba80e0af618660c20c314f9ab7
Chris Penner <christopher.penner@gmail.com>
2017-12-03 17:21:25 -0600

    Correct dumbwaiter sha sum

--- dumbwaiter.rb
+++ dumbwaiter.rb
@@ -2,7 +2,7 @@ class Dumbwaiter < Formula
   desc "A dead-simple server configurable via yaml file"
   homepage "https://github.com/ChrisPenner/dumbwaiter"
   url "https://github.com/ChrisPenner/dumbwaiter/releases/download/v0.2.2.0/dumbwaiter-v0.2.2.0-osx.tar.gz"
-  sha256 "b737d33a64810825e2671d0e3e9b019d021cdee4"
+  sha256 "9bef10e6d0297bfcd3c6e528baa96f053ab657244d307845283a5e6310803cb8"
 
   bottle :unneeded
 
