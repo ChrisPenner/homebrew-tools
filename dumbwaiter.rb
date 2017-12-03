403a234e25704436b33c3cd0bb777b3e88b4c4d3
Chris Penner <christopher.penner@gmail.com>
2017-12-03 17:19:14 -0600

    Fix incorrect link for dumbwaiter

--- dumbwaiter.rb
+++ dumbwaiter.rb
@@ -1,7 +1,7 @@
 class Dumbwaiter < Formula
   desc "A dead-simple server configurable via yaml file"
   homepage "https://github.com/ChrisPenner/dumbwaiter"
-  url "https://github.com/ChrisPenner/tempered/releases/download/v0.2.2.0/dumbwaiter-v0.2.2.0-osx.tar.gz"
+  url "https://github.com/ChrisPenner/dumbwaiter/releases/download/v0.2.2.0/dumbwaiter-v0.2.2.0-osx.tar.gz"
   sha256 "b737d33a64810825e2671d0e3e9b019d021cdee4"
 
   bottle :unneeded
